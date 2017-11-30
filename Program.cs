f0fc13f0c71170a810800abe5211357015827a54
aldrindlc <33703082+aldrindlc@users.noreply.github.com>
2017-11-30 14:55:46 -0700

    wd

--- Program.cs
+++ Program.cs
@@ -6,10 +6,46 @@ namespace AldrinD1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Write 2 numers");
+            Console.WriteLine("Write 2 numbers");
             float number1 = float.Parse(Console.ReadLine());
             float number2 = float.Parse(Console.ReadLine());
-            Console.WriteLine(number1 + number2);
+
+
+            Console.WriteLine("Select an option:\n1.-Add\n2.-Subtract\n3.-Multiply\n4.-Divide");
+
+            int option = int.Parse(Console.ReadLine());
+
+
+            EvaluateOption(option, number1, number2);
         }
+
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
+        }
+
+        public static void EvaluateOption(int option, float n1, float n2)
+        {
+            else if (option == 1)
+            {
+                Console.WriteLine("Result of addition: " + AddTwoNumbers(n1, n2));
+            }
+			else if (option == 2)
+			{
+				Console.WriteLine("Result of subtraction: " + AddTwoNumbers(n1, n2));
+			}
+
+			else if (option == 3)
+			{
+				Console.WriteLine("Result of multiplication: " + AddTwoNumbers(n1, n2));
+			}
+			else if (option == 4)
+			{
+				Console.WriteLine("Result of division: " + AddTwoNumbers(n1, n2));
+			}
+
+		}
     }
 }
