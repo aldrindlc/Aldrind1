195ee1aaa88c22b0637786637b552290716a9818
aldrindlc <33703082+aldrindlc@users.noreply.github.com>
2017-11-30 14:20:01 -0700

    ee

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace AldrinD1
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
         }
     }
 }
